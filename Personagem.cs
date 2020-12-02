de29a37747bab16eada085e7b6573d4685c726dd
Kemilly Ferreira <73208400+KemillyFerreira@users.noreply.github.com>
2020-12-02 01:38:48 -0300

    Add files via upload

---
+++ Personagem.cs
@@ -0,0 +1,19 @@
+namespace AulaDois
+{
+    public class Personagem
+    {
+        public string nome = "Tony Stark";
+
+        public int idade = 40;
+        public string armadura;
+
+        public string Atacar (){
+            return "Ataque de fogo!";          
+        }
+
+        public string Defender(){
+            return "Não foi dessa vez!";
+        }
+            
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AulaDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem objetoPersonagem = new Personagem();
+
+            objetoPersonagem.nome = "Kali";
+            objetoPersonagem.idade = 19;
+            objetoPersonagem.armadura = "Voadora";
+
+
+
+            Personagem objetoPersonagem2 = new Personagem();
+
+            objetoPersonagem2.nome = "Kemilly";
+            objetoPersonagem2.idade = 20;
+            objetoPersonagem2.armadura = "Flash"; 
+
+            Console.WriteLine($"Personagem 1 = {objetoPersonagem.nome} - {objetoPersonagem.Atacar()}");
+            Console.WriteLine($"Personagem 2 = {objetoPersonagem2.nome} - {objetoPersonagem2.Defender()}");
+
+        }                         
+    }
+}
+ 
